61fd54e524f08a625ec078d3bb847cc60aa28c01
Pahud <pahudnet@gmail.com>
2021-04-07 08:44:43 +0800

    add projen-automate-it

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,5 @@
 const { AwsCdkTypeScriptApp } = require('projen');
+const { Automation } = require('projen-automate-it');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
@@ -19,55 +20,18 @@ const project = new AwsCdkTypeScriptApp({
     '@aws-cdk/aws-elasticloadbalancingv2',
     '@aws-cdk/aws-secretsmanager',
   ],
+  devDeps: ['projen-automate-it'],
   dependabot: false,
-  defaultReleaseBranch: 'master',
+  defaultReleaseBranch: 'main',
 });
 
-project.addFields({
-  resolutions: {
-    netmask: '2.0.1',
-  },
+const automation = new Automation(project, {
+  automationToken: AUTOMATION_TOKEN,
 });
 
-// create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '0 6 * * *',
-  }], // 6am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
+automation.projenYarnUpgrade();
+automation.autoApprove();
+automation.autoMerge();
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log'];
 project.npmignore.exclude('images', ...common_exclude);
